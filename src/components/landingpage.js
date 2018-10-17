63af1ded8ac50cc2593dba77abea5bbe519e51af
Eerik Lehtomäki <eejujole@student.jyu.fi>
2018-10-18 00:47:59 +0300

    frontpage almost done

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+import image from '../images/avatar.png';
 
 class Landing extends Component {
     render() {
         return(
-            <div>
-                <h1>Landing Page</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img
+                            src={image}
+                            alt="avatar"
+                            className="avatar-img"
+                        />
+                        <div className="banner-text">
+                            <h1>Program Developer</h1>
+
+                            <hr />
+
+                            <p>HTML/CSS | Bootstrap | React | NodeJS | Javascript </p>
+
+                            <div className="social-links">
+
+                                <a href="http://www.googlw.com"  rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true" />
+                                </a>
+
+                                <a href="http://www.googlw.com"  rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-github-square" aria-hidden="true" />
+                                </a>
+
+                                <a href="http://www.googlw.com"  rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-youtube-square" aria-hidden="true" />
+                                </a>
+
+                            </div>
+                        </div>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
